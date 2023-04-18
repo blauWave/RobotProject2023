32283f61054f1cc2c509c841568910d22cffeee5
bluaWave <maviwave@gmail.com>
2023-04-18 18:43:04 +0200

    added new files

--- Libraries/Driver_Manager.py
+++ Libraries/Driver_Manager.py
@@ -1,7 +1,7 @@
 from robot.api.deco import keyword
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
-from webdriver_manager.microsoft import IEDriverManager
+from webdriver_manager.microsoft import IEDriverManager, EdgeChromiumDriverManager
 # funktioniert nicht auf Windows-Server, weil  cannot import name 'EdgeChromiumDriverManager' from 'webdriver_manager.microsoft' (C:\Users\WD10012\AppData\Roaming\Python\Python310\site-packages\webdriver_manager\microsoft.py)
 # from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from webdriver_manager.opera import OperaDriverManager
